3b8890b33b46e52a98a8f07ecf2318b7076e6020
Mathieu Mence <Mathieu.Mence@matnard.com>
2019-09-09 11:25:01 +1000

    refactors lab page

--- src/pages/lab.js
+++ src/pages/lab.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { graphql } from "gatsby";
 import styled from "@emotion/styled";
 import SEO from "../components/seo";
@@ -7,76 +7,72 @@ import { css } from "@emotion/core";
 import Img from "../components/image";
 import mq from "../layouts/breakpoints";
 
-class Lab extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projects: []
-    };
-  }
+function Lab(props) {
+  const Div = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: var(--baseline);
+  `;
 
-  render() {
-    const Div = styled.div`
-      display: grid;
+  const Feats = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: var(--baseline);
+
+    ${mq[0]} {
       grid-template-columns: 1fr 1fr 1fr;
-      grid-gap: var(--baseline);
-    `;
+    }
+  `;
 
-    const Feats = styled.div`
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-gap: var(--baseline);
+  const labLinkStyles = css`
+    text-decoration: none;
+    text-transform: capitalize;
+    color: var(--color-text);
 
-      ${mq[0]} {
-        grid-template-columns: 1fr 1fr 1fr;
-      }
-    `;
-
-    const labLinkStyles = css`
-      text-decoration: none;
+    &:visited {
       color: var(--color-text);
+    }
 
-      &:visited {
-        color: var(--color-text);
-      }
-    `;
+    .gatsby-image-wrapper {
+      margin-bottom: 12px;
+    }
+  `;
 
-    return (
-      <>
-        <SEO
-          title="Lab"
-          keywords={[`experiments`, `javascript`, `frontend`, `webgl`]}
-        />
-        <h3>Lab</h3>
-        <Feats>
+  return (
+    <>
+      <SEO
+        title="Lab"
+        keywords={[`experiments`, `javascript`, `frontend`, `webgl`]}
+      />
+      <h3>Lab</h3>
+      <Feats>
+        <a
+          css={labLinkStyles}
+          href="https://github.com/Matnard/NardGL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Img filename="nardgl.png" />
+          {"Open-source graphics library"}
+        </a>
+      </Feats>
+      <br />
+      <Div>
+        {props.data.allMatnardProjects.edges.map(({ node }) => (
           <a
             css={labLinkStyles}
-            href="https://github.com/Matnard/NardGL"
+            key={node.id}
+            href={node.url}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Img filename="nardgl.png" />
-            {"Open-source graphics library"}
+            {<Img src={node.screenshot_url} alt="" />}
+            {node.name}
           </a>
-        </Feats>
-        <br />
-        <Div>
-          {this.props.data.allMatnardProjects.edges.map(({ node }) => (
-            <a
-              css={labLinkStyles}
-              key={node.id}
-              href={node.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {<Img src={node.screenshot_url} alt="" />}
-              {node.name}
-            </a>
-          ))}
-        </Div>
-      </>
-    );
-  }
+        ))}
+      </Div>
+    </>
+  );
 }
 
 export default Lab;
