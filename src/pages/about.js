46a1a7691a84ab54f65d2217d68b1fdf62e039cb
Mathieu Mence <mathieu@matnard.com>
2022-03-15 18:55:28 +1100

    bio update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Row } from "../layouts/index";
 import styled from "@emotion/styled";
+import React from "react";
 import SEO from "../components/seo";
+import { Row } from "../layouts/index";
 
 const Section = styled.section`
   grid-column-start: 1;
@@ -19,54 +19,33 @@ const AboutPage = () => (
         `fullstack`,
         `c#`,
         `dotnet core`,
-        `javascript`
+        `javascript`,
       ]}
     />
     <Row>
       <Section>
         <h2>Hi!</h2>
         <p>
-          I am a French-Caribbean Fullstack Engineer living permanently in
-          Sydney Australia and{" "}
-          {/*<strike>currently working at whiteGREY</strike>*/}
-          <strike>currently working at The Monkeys.</strike> currently working
-          at MYOB
+          I am a French-Caribbean Fullstack Engineer residing in Sydney,
+          Australia. I've been coding since high school and professionally since
+          2010, after landing my first job in Toronto Canada as a Junior Flash
+          Developer.
         </p>
         <p>
-          {/*<strike>Here I create, maintain websites and JavaScript applications with
-          React, Emotion and more recently serverless GatsbyJS websites
-          extending WordPress v5 Gutenberg's blocks.</strike>*/}
-          <strike>
-            Here, with my React and PixiJs skills I'm helping planting trees by
-            coding a website that creates awareness to get koalas{" "}
-            <span role="img" aria-label="koala">
-              🐨
-            </span>{" "}
-            better homes!
-          </strike>
-          <br />
-          Here, I am part of a feature team that is working on a ReactJS C#
-          Dotnet Core stack in a microservices architecture where we're enabling
-          New Zealand taxes to be "filed" in the cloud{" "}
-          <span role="img" aria-label="cloud">
-            🌥️
-          </span>
-          .
+          Currently, I am part of a dedicated product team at Playground XYZ,
+          where we are working on a ReactJS TypeScript stack in a micro-services
+          architecture that enables our clients to get deep insights on how
+          users engage with their advertisements.
         </p>
         <p>
-          I've studied analogue and digital electronics and really enjoy the
-          low-level of things. I've been coding since high school and
-          professionally since 2010, after landing my first job in Toronto
-          Canada as a Junior Flash Developer.
+          I've studied both analogue and digital electronics to feed my interest
+          in the granular aspects of software engineering.
         </p>
         <p>
-          Data to Pixels/Texels is the side I'm interested in, that's why I
-          spend time studying graphics shader programming and design principles
-          to eventually make stuff "look good" and "feel good"{" "}
-          <span role="img" aria-label="sweat">
-            😅
-          </span>
-          .
+          A key driver in my work is to make functional software that looks and
+          feels good to use. I have a keen eye for design and functionality. My
+          interest in Data to Pixels/Texels led me to study graphics shader
+          programming and design principles in my spare time.
         </p>
       </Section>
     </Row>
