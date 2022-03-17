ec7da1d8b1f2b9330c9914f787c7ad636ccd52bb
Mathieu Mence <mathieu@matnard.com>
2022-03-17 17:03:22 +1100

    bio update

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,26 +26,30 @@ const AboutPage = () => (
       <Section>
         <h2>Hi!</h2>
         <p>
-          I am a French-Caribbean Fullstack Engineer residing in Sydney,
-          Australia. I've been coding since high school and professionally since
-          2010, after landing my first job in Toronto Canada as a Junior Flash
-          Developer.
+          I am a French-Caribbean Web Graphics and Fullstack Engineer residing
+          in Sydney, Australia. I've been coding since high school and
+          professionally since 2010, after landing my first job in Toronto
+          Canada as a Junior Flash Developer.
         </p>
         <p>
-          Currently, I am part of a dedicated product team at Playground XYZ,
-          where we are working on a ReactJS TypeScript stack in a micro-services
-          architecture to enable our clients to get deep insights on how users
-          engage with their advertisements.
+          Currently, I am a Software Engineer at Playground XYZ. My role
+          includes the development and maintenance of React/TypeScript
+          microservices hosted on Google Cloud Platform.
+        </p>
+        <p>
+          Previously, I was part of a special project team at MYOB, where we
+          worked on a ReactJS C# Dotnet Core stack in a micro-services
+          architecture for the New Zealand feature of MYOB's online accounting
+          software.
         </p>
         <p>
           I've studied both analogue and digital electronics to feed my interest
           in the granular aspects of software engineering.
         </p>
-
         <p>
           A key driver in my work is to make functional software that looks and
-          feels good to use. I have a keen eye for design and functionality. My
-          interest in Data to Pixels/Texels led me to study graphics shader
+          feels good to use. I have a keen eye for design and functionality, and
+          my interest in Data to Pixels/Texels led me to study graphics shader
           programming and design principles in my spare time.
         </p>
       </Section>
